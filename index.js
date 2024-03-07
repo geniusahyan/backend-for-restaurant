d0fda3555222d979d4084a3e5a80c99647bb1d20
aslam-ahyan <alamaslam078523@gmail.com>
2024-03-07 16:21:43 +0530

    remove cors

--- index.js
+++ index.js
@@ -12,20 +12,7 @@ app.get('/', (req,res)=>{
     res.send("<h2>this is something</h2>");
 })
 
-const allowedOrigins = ['http://localhost:5173'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
-
+app.use(cors())
 app.use(bodyParser.json({exented:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 
